89736140d1d835d5185f4c07564dafcf574962b2
naemcivic <naemcivic@gmail.com>
2017-06-02 08:09:54 -0400

    start building Stocks and Stock component

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,9 +1,26 @@
 <template lang="html">
-  <h1>The Stocks Component</h1>
+  <div>
+    <app-stock v-for="stock in stocks"></app-stock>
+  </div>
 </template>
 
 <script>
+import Stock from './Stock.vue';
+
 export default {
+  data () {
+    return {
+      stocks: [
+        { id: 1, name: 'BMW', price: 110 },
+        { id: 1, name: 'Google', price: 200 },
+        { id: 1, name: 'Apple', price: 250 },
+        { id: 1, name: 'Twitter', price: 110 }
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
 }
 </script>
 
